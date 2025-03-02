776e3ab4e8cf90ad6f5fe9b12ab4d59ab226a74f
arjunverma06 <arjunverma5319@gmail.com>
2025-03-02 14:47:16 +0530

    some changes

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from typing import List
+def get_requirements() -> List[str]:
+    list_requirements=list[str]=[]
+    return list_requirements
+
+
+setup(
+    name='Sensor_project',
+    version='1.6.3',
+    author="Arjun",
+    author_email="arjun225553@gmail.com",
+    packages=find_packages(),
+    install_reuires=get_requirements(),
+)
